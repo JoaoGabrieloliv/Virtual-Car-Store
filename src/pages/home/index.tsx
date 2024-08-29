5092934848e2e934e6f9324eabf92f59b9fc11cb
JOAO GABRIEL OLIVEIRA SOUZA LIMA SALES <joao.sales@demprel.local>
2024-08-29 15:18:28 -0300

    JoaoCommit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,7 +8,7 @@ export function Home() {
           className="w-full border-2 rounded-lg h-9 px-3 outline-none"
           placeholder="Digite o nome do carro..."
         />
-        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
+          <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">
           Buscar
         </button>
       </section>
