62591cf96e52b303c0e4ed9d2dac8d7a4a3d02eb
João Gabriel <joaoglimasales@gmail.com>
2024-10-30 00:19:03 -0300

    Wanjo Max joao gabriel

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { ReactNode, createContext, useState, useEffect } from "react";
-import { string } from "zod";
+
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../services/firebaseConnection";
 
